ba22a8010fd7791e28c70409b5166cfb9a832e94
ehmicky <ehmicky@gmail.com>
2018-10-16 17:00:00 +0200

    Add comment

--- src/log.js
+++ src/log.js
@@ -2,6 +2,8 @@
 
 // Default `opts.log`
 const defaultLog = function(message, level) {
+  // Note that `console` should be referenced inside this function, not outside,
+  // as user might monkey patch it.
   // eslint-disable-next-line no-restricted-globals, no-console
   console[level](message)
 }

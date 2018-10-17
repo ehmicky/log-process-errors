d4f845cd12572946c5eca3be8800970596809d73
ehmicky <ehmicky@gmail.com>
2018-10-17 17:00:00 +0200

    Change how module is required

---
+++ custom.js
@@ -0,0 +1,4 @@
+'use strict'
+
+// See `index.js` for an alternative to this file
+module.exports = require('./src')
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict'
 
-module.exports = require('./src')
+const setup = require('./src')
+
+// Logging setup is automatically performed by `node -r log-process-errors` or
+// `require('log-process-errors')`.
+// It encourages using `node -r` to prevent from adding this helper in the
+// source code. This is beneficial in the following use cases:
+//  - when using it for debugging purpose only
+//  - discouraging libraries from shipping this code, as it mutates global
+//    environment. This could lead to unexpected side-effects when requiring
+//    the library, and to conflicts between libraries.
+// Also it ensures this code is called as early as possible, to make sure early
+// events are caught.
+// To either defer the call or to pass options, one must use instead
+// `require('log-process-errors/custom')(options)`
+setup()

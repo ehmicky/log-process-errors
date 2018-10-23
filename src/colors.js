17eac728c3448f313fa42aabf1b606dd3b4a287b
ehmicky <ehmicky@gmail.com>
2018-10-23 17:00:00 +0200

    Fix opts.colors

--- src/colors.js
+++ src/colors.js
@@ -3,11 +3,11 @@
 const chalk = require('chalk')
 
 const getColors = function({ opts: { colors } }) {
-  // Can disable colors with `opts.colors: false`.
+  // Can disable or force colors with `opts.colors`.
   // chalk will automatically disable colors if output does not support it.
-  const enabled = Boolean(colors)
+  const chalkOpts = colors === undefined ? {} : { enabled: Boolean(colors) }
 
-  return chalk.constructor({ enabled })
+  return chalk.constructor(chalkOpts)
 }
 
 module.exports = {

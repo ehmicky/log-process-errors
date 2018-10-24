ee2c297d98a33d0002d89122722bb44fe4e7c97b
ehmicky <ehmicky@gmail.com>
2018-10-24 17:00:00 +0200

    Remove unreacheable code

--- src/colors.js
+++ src/colors.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 const getColors = function({ opts: { colors } }) {
   // Can disable or force colors with `opts.colors`.
   // chalk will automatically disable colors if output does not support it.
-  const chalkOpts = colors === undefined ? {} : { enabled: Boolean(colors) }
+  const chalkOpts = { enabled: Boolean(colors) }
 
   return chalk.constructor(chalkOpts)
 }

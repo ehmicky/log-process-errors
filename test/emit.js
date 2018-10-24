f7bf799ef05c1f2ed2cfe8c65e4581b7053ad16d
ehmicky <ehmicky@gmail.com>
2018-10-24 17:00:00 +0200

    Add test

--- test/emit.js
+++ test/emit.js
@@ -1,8 +1,23 @@
 'use strict'
 
+const test = require('ava')
+
+const {
+  ALL_EVENTS: { all },
+} = require('../helpers')
+
 const { forEachEvent, startLogging } = require('./helpers')
 
+test('[all] events emitters should not throw', async t => {
+  const { stopLogging } = startLogging()
+
+  await t.notThrowsAsync(all)
+
+  stopLogging()
+})
+
 /* eslint-disable max-nested-callbacks */
+// eslint-disable-next-line no-shadow
 forEachEvent(({ emitEvent, test }) => {
   test('events emitters should exist', t => {
     t.is(typeof emitEvent, 'function')

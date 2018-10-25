e9d1ef5fe73fb9eac156efb5eef293a78016a263
ehmicky <ehmicky@gmail.com>
2018-10-25 17:00:00 +0200

    Fix `opts.colors: true`

--- src/colors.js
+++ src/colors.js
@@ -1,13 +1,28 @@
 'use strict'
 
 const chalk = require('chalk')
+const { stdout: { level } = {} } = require('supports-color')
 
-const getColors = function({ opts: { colors } }) {
-  // Can disable or force colors with `opts.colors`.
-  // chalk will automatically disable colors if output does not support it.
-  const chalkOpts = { enabled: Boolean(colors) }
-
+const getColors = function({ opts }) {
+  const chalkOpts = getChalkOpts({ opts })
   return chalk.constructor(chalkOpts)
+}
+
+// Can disable or force colors with `opts.colors`.
+// chalk will automatically disable colors if output does not support it.
+const getChalkOpts = function({ opts: { colors } }) {
+  if (colors === undefined) {
+    return {}
+  }
+
+  if (colors === false) {
+    return { enabled: false }
+  }
+
+  // Chalk `level` must be set otherwise `enabled` will be noop if terminal
+  // does not support color (e.g. during a shell file redirection)
+  // However it should only be set if it is `undefined` or `0`
+  return { enabled: true, level: level || 2 }
 }
 
 module.exports = {

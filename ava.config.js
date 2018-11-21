37695efdf05a5221797ed57df0ce8f28a84286af
ehmicky <ehmicky@gmail.com>
2018-11-21 19:45:06 +0100

    Fix tests

--- ava.config.js
+++ ava.config.js
@@ -1,6 +1,9 @@
 'use strict'
 
 export default {
+  babel: false,
+  compileEnhancements: false,
+  files: ['dist/test'],
   // Tests are setting listeners on `process` and stubbing global
   // `process.exit`, `console`, `setTimeout` and `Error.prepareStackTrace`,
   // so we cannot parallelize them.

390976cae302b0c7673fcc9219456129d782beff
ehmicky <ehmicky@gmail.com>
2018-11-22 10:02:28 +0100

    Start fixing Ava

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,24 +3,20 @@
 const { series } = require('gulp')
 
 const { getWatchTask } = require('../utils')
-const FILES = require('../files')
+const { BUILD_SRC, BUILD_DIST } = require('../files')
 const gulpExeca = require('../exec')
 
-const babel = function() {
-  const promises = FILES.BUILD.map(dir =>
-    gulpExeca(
-      `babel ${dir} --out-dir dist/${dir} --copy-files --delete-dir-on-start --source-maps --no-comments --minified --retain-lines`,
-    ),
+const babel = () =>
+  gulpExeca(
+    `babel ${BUILD_SRC} --out-dir ${BUILD_DIST} --copy-files --delete-dir-on-start --source-maps --no-comments --minified --retain-lines`,
   )
-  return Promise.all(promises)
-}
 
 const build = series(babel)
 
 // eslint-disable-next-line fp/no-mutation
 build.description = 'Build source files'
 
-const buildwatch = getWatchTask({ BUILD: build }, build)
+const buildwatch = getWatchTask({ BUILD_SRC: build }, build)
 
 // eslint-disable-next-line fp/no-mutation
 buildwatch.description = 'Build source files in watch mode'

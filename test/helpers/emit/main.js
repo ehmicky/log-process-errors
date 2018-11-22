72499dad7c7839ded18a3ba2fa9d9495ca307677
ehmicky <ehmicky@gmail.com>
2018-11-22 10:26:53 +0100

    Fix code duplication

--- test/helpers/emit/main.js
+++ test/helpers/emit/main.js
@@ -5,7 +5,6 @@ const { unhandledRejection } = require('./unhandled_rejection')
 const { rejectionHandled } = require('./rejection_handled')
 const { multipleResolves } = require('./multiple_resolves')
 const { warning } = require('./warning')
-const { all } = require('./all')
 
 const EVENTS = {
   uncaughtException,
@@ -15,9 +14,19 @@ const EVENTS = {
   warning,
 }
 
+// Emit all process error events
+const all = async function() {
+  // eslint-disable-next-line fp/no-loops
+  for (const emitEvent of Object.values(EVENTS)) {
+    // eslint-disable-next-line no-await-in-loop
+    await emitEvent()
+  }
+}
+
 const ALL_EVENTS = { ...EVENTS, all }
 
 module.exports = {
+  all,
   EVENTS,
   ALL_EVENTS,
 }

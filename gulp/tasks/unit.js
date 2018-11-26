8288dbbf98963a717881769b24e7a561ce4f55d3
ehmicky <ehmicky@gmail.com>
2018-11-26 17:48:02 +0100

    Add comments

--- gulp/tasks/unit.js
+++ gulp/tasks/unit.js
@@ -13,16 +13,27 @@ const gulpExeca = require('../exec')
 const TRAVIS_CONFIG = `${__dirname}/../../.travis.yml`
 
 const unit = async function() {
+  // In CI, we use `pack`, but not locally since it is slow.
+  // Also, in CI we do test coverage and send it to Coveralls.
   if (!isCi) {
     return gulpExeca('ava')
   }
 
+  // When using `pack`, tested files will be inside `node_modules`
+  // By default `nyc` ignore those, so we need to add them to `--include``
+  // Even after this, `node_modules` are still ignored by `nyc` unless using
+  // a negated `--exclude`
   await pack(`nyc --include ${NESTED_DIR} --exclude !${NESTED_DIR} ava`)
 
   await sendToCoveralls()
 }
 
+// eslint-disable-next-line fp/no-mutation
+unit.description = 'Run unit tests'
+
 const sendToCoveralls = async function() {
+  // We strip `node_modules/PACKAGE/` from test coverage reports so it looks
+  // like source files were in the same directory (not inside `node_modules`).
   const covMap = await promisify(readFile)(COVMAP_PATH, { encoding: 'utf-8' })
   const covMapA = covMap.replace(NESTED_DIR_REGEXP, '')
 
@@ -30,14 +41,12 @@ const sendToCoveralls = async function() {
 }
 
 const NESTED_DIR = `node_modules/${name}`
+// The RegExp needs to account for Windows having different separators.
 const NESTED_DIR_REGEXP = new RegExp(
   `node_modules(\\/|\\\\)${name}(\\/|\\\\)`,
   'gu',
 )
 const COVMAP_PATH = './coverage/lcov.info'
-
-// eslint-disable-next-line fp/no-mutation
-unit.description = 'Run unit tests'
 
 // We have to use this to debug Ava test files with Chrome devtools
 const unitwatch = getWatchTask({ UNIT: unit }, unit)

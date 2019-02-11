a4cfcc211360df060534b7418854cc6770e76af1
ehmicky <ehmicky@gmail.com>
2019-02-11 10:00:00 +0100

    Remove @babel/register hack

--- test/helpers/simple_loader.js
+++ test/helpers/simple_loader.js
@@ -2,13 +2,14 @@
 
 const { argv } = require('process')
 
-// While Ava uses Babel by default, it does not do it on child processes,
-// i.e. this is needed.
-// eslint-disable-next-line import/no-unassigned-import
-require('@babel/register')
-
 // eslint-disable-next-line import/no-unassigned-import
 require('../../register')
+
+// While Ava uses Babel by default, it does not do it on child processes,
+// i.e. this is needed.
+// It needs to be performed after source files are required.
+// eslint-disable-next-line import/no-unassigned-import
+require('@babel/register')
 
 const { stubStackTrace } = require('./stack')
 const { EVENTS } = require('./emit')

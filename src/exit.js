dd0214a946dddac9fa4bf9100f51f80dd32b45a6
ehmicky <ehmicky@gmail.com>
2019-02-11 10:00:00 +0100

    Fix line width

--- src/exit.js
+++ src/exit.js
@@ -10,7 +10,8 @@ const {
 const { EXIT_STATUS, EXIT_TIMEOUT } = require('./constants')
 
 // Exit process according to `opts.exitOn` (default: ['uncaughtException']):
-//  - `uncaughtException`: default behavior of Node.js and recommended by https://nodejs.org/api/process.html#process_warning_using_uncaughtexception_correctly
+//  - `uncaughtException`: default behavior of Node.js and recommended by
+//     https://nodejs.org/api/process.html#process_warning_using_uncaughtexception_correctly
 //  - `unhandledRejection`: possible future behavior and recommended by Node.js.
 //    See https://nodejs.org/dist/latest-v8.x/docs/api/deprecations.html#deprecations_dep0018_unhandled_promise_rejections
 // By default `unhandledRejection` is opt-in so that using this library does not

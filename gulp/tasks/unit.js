97808ed3b3bbfcac340cee5b4a64920b5b7606a7
ehmicky <ehmicky@gmail.com>
2019-02-17 10:00:00 +0100

    Remove UNIT

--- gulp/tasks/unit.js
+++ gulp/tasks/unit.js
@@ -34,7 +34,7 @@ const PLATFORMS = {
 unit.description = 'Run unit tests'
 
 // We have to use this to debug Ava test files with Chrome devtools
-const unitwatch = getWatchTask({ UNIT: unit }, unit)
+const unitwatch = getWatchTask({ CHECK: unit }, unit)
 
 // eslint-disable-next-line fp/no-mutation
 unitwatch.description = 'Run unit tests in watch mode'

b0de5b0976f5a23e3fe79fc3b1f326fb8d27a4df
ehmicky <ehmicky@gmail.com>
2019-02-19 15:28:23 +0100

    Use shared Babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,3 @@
-// istanbul ignore file
 'use strict'
 
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: { node: '6.5.0' },
-        useBuiltIns: 'usage',
-      },
-    ],
-  ],
-  plugins: ['@babel/plugin-transform-runtime'],
-  shouldPrintComment: comment => comment.includes('istanbul ignore'),
-}
+module.exports = require('gulp-shared-tasks/.babelrc.js')

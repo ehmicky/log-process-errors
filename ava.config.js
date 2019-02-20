c26ec8bbe030bb6d1630d3700dce03e80105c523
ehmicky <ehmicky@gmail.com>
2019-02-20 10:00:00 +0100

    Put ava configuration in package.json

--- ava.config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-export default {
-  // Tests are setting listeners on `process` and stubbing global
-  // `process.exit`, `console`, `setTimeout` and `Error.prepareStackTrace`,
-  // so we cannot parallelize them.
-  serial: true,
-}

67b37cd3edbc4f4282de809226944a193e302968
ehmicky <ehmicky@gmail.com>
2019-03-04 10:00:00 +0100

    Remove dependencies

--- test/helpers/noop_loader.js
+++ test/helpers/noop_loader.js
@@ -3,12 +3,6 @@
 // eslint-disable-next-line import/no-unassigned-import, node/no-missing-require, import/no-unresolved
 require('../../../register')
 
-// While Ava uses Babel by default, it does not do it on child processes,
-// i.e. this is needed.
-// It needs to be performed after source files are required.
-// eslint-disable-next-line import/no-unassigned-import
-require('@babel/register')
-
 const logProcessErrors = require('../../src')
 
 const stopLogging = logProcessErrors()
--- test/helpers/simple_loader.js
+++ test/helpers/simple_loader.js
@@ -5,12 +5,6 @@ const { argv } = require('process')
 // eslint-disable-next-line import/no-unassigned-import, node/no-missing-require, import/no-unresolved
 require('../../../register')
 
-// While Ava uses Babel by default, it does not do it on child processes,
-// i.e. this is needed.
-// It needs to be performed after source files are required.
-// eslint-disable-next-line import/no-unassigned-import
-require('@babel/register')
-
 const { stubStackTrace } = require('./stack')
 const { EVENTS } = require('./emit')
 

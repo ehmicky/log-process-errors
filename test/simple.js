c18c56f4239359b2d338bc1b345ea1dbcd8fc7a7
ehmicky <ehmicky@gmail.com>
2019-03-26 10:00:00 +0100

    Improve test

--- test/simple.js
+++ test/simple.js
@@ -5,43 +5,33 @@ const execa = require('execa')
 
 const { repeatEvents, normalizeMessage } = require('./helpers')
 
+const SIMPLE_LOADER = `${__dirname}/helpers/simple_loader`
+const NOOP_LOADER = `${__dirname}/helpers/noop_loader`
+
 repeatEvents((prefix, { name }) => {
   test(`${prefix} should work using the -r flag`, async t => {
-    const returnValue = await callLoader({ name, loader: 'simple' })
+    const returnValue = await callLoader([SIMPLE_LOADER, name])
 
     t.snapshot(returnValue)
   })
 
   test(`${prefix} should work with --no-warnings`, async t => {
-    const returnValue = await callLoader({
-      name,
-      loader: 'simple',
-      flags: ['--no-warnings'],
-    })
+    const returnValue = await callLoader([SIMPLE_LOADER, name, '--no-warnings'])
 
     t.snapshot(returnValue)
   })
 
   test(`${prefix} should work using both the -r flag and init()`, async t => {
-    const returnValue = await callLoader({ name, loader: 'noop' })
+    const returnValue = await callLoader([NOOP_LOADER, name])
 
     t.snapshot(returnValue)
   })
 })
 
-const callLoader = async function({ name, loader, flags = [] }) {
-  const { stdout, stderr, code } = await execa(
-    'node',
-    [...flags, LOADERS[loader], name],
-    { reject: false },
-  )
+const callLoader = async function(args) {
+  const { stdout, stderr, code } = await execa('node', args, { reject: false })
 
   const stdoutA = normalizeMessage(stdout)
   const stderrA = normalizeMessage(stderr)
   return { code, stdout: stdoutA, stderr: stderrA }
-}
-
-const LOADERS = {
-  simple: `${__dirname}/helpers/simple_loader`,
-  noop: `${__dirname}/helpers/noop_loader`,
 }

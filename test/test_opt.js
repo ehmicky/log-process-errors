d3c20427744acf8a3594075e4a79978b80a4c411
ehmicky <ehmicky@gmail.com>
2019-03-27 10:00:00 +0100

    Refactor test

--- test/test_opt.js
+++ test/test_opt.js
@@ -8,35 +8,38 @@ const HELPER_DIR = `${__dirname}/helpers/test_opt`
 
 repeatEventsRunners((prefix, testRunner, { name }) => {
   test(`${prefix} should make tests fails`, async t => {
-    const helperFile = getHelperFile(testRunner)
-    const options = { name, test: testRunner }
-    const returnValue = await normalizeCall(testRunner, [helperFile], {
-      env: { OPTIONS: JSON.stringify(options) },
-    })
+    const returnValue = await callRunner({ testRunner, name })
 
     t.snapshot(returnValue)
   })
 
   test(`${prefix} should allow overriding 'opts.level'`, async t => {
-    const helperFile = getHelperFile(testRunner)
-    const options = { name, test: testRunner, level: { default: 'silent' } }
-    const returnValue = await normalizeCall(testRunner, [helperFile], {
-      env: { OPTIONS: JSON.stringify(options) },
-    })
+    const returnValue = await callRunner(
+      { testRunner, name },
+      { level: { default: 'silent' } },
+    )
 
     t.snapshot(returnValue)
   })
 
   test(`${prefix} should work with the -r flag`, async t => {
-    const helperFile = getHelperFile(testRunner)
-    const options = { name, test: testRunner, register: true }
-    const returnValue = await normalizeCall(testRunner, [helperFile], {
-      env: { OPTIONS: JSON.stringify(options) },
-    })
+    const returnValue = await callRunner(
+      { testRunner, name },
+      { register: true },
+    )
 
     t.snapshot(returnValue)
   })
 })
+
+const callRunner = async function({ testRunner, name }, opts) {
+  const helperFile = getHelperFile(testRunner)
+  const options = { name, test: testRunner, ...opts }
+  const returnValue = await normalizeCall(testRunner, [helperFile], {
+    env: { OPTIONS: JSON.stringify(options) },
+  })
+  return returnValue
+}
 
 const getHelperFile = function(testRunner) {
   // TODO: remove next comment once we support over test runners than 'ava'

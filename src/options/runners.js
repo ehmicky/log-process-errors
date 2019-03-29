d76293a7a36c0be92ed4550d099ec898ed3dad2d
ehmicky <ehmicky@gmail.com>
2019-03-29 10:00:00 +0100

    Refactoring

--- src/options/runners.js
+++ src/options/runners.js
@@ -4,35 +4,32 @@ const { nextTick } = require('process')
 
 // Make `opts.log()` propagate an `uncaughtException` so that test runner
 // reports the original process error as a test failure.
-// `message` is a stringified `Error`. We parse it back to an `Error` object.
-const propagateError = function(message) {
+const throwUncaughtException = function(message) {
   nextTick(() => {
-    const [messageA, ...stack] = message.split('\n')
-    const stackA = stack.join('\n')
-    const error = new Error(messageA)
-    // eslint-disable-next-line fp/no-mutation
-    error.stack = stackA
+    const error = messageToError({ message })
     throw error
   })
 }
 
-// Some test runners like Jasmine print both `Error.message` and `Error.stack`,
-// which leads to duplicate stack traces.
-const propagateString = function(message) {
-  nextTick(() => {
-    throw message
-  })
+// `message` is a stringified `Error`. We parse it back to an `Error` object.
+const messageToError = function({ message }) {
+  const [messageA, ...stack] = message.split('\n')
+  const stackA = stack.join('\n')
+  const error = new Error(messageA)
+  // eslint-disable-next-line fp/no-mutation
+  error.stack = stackA
+  return error
 }
 
 // Options common to most runners
 const COMMON_OPTIONS = {
-  log: propagateError,
+  log: throwUncaughtException,
   // Most runners do their own colorization
   colors: false,
   // Other tests should keep running
   exitOn: [],
   // All runners need to report `uncaughtException` for `propagateError()` to
-  // work. Most also report `unhandledRejection`
+  // work. Most also report `unhandledRejection`.
   level: { uncaughtException: 'silent', unhandledRejection: 'silent' },
 }
 
@@ -40,7 +37,7 @@ const RUNNERS = {
   ava: COMMON_OPTIONS,
   // Mocha does not report `unhandleRejection`
   mocha: { ...COMMON_OPTIONS, level: { uncaughtException: 'silent' } },
-  jasmine: { ...COMMON_OPTIONS, log: propagateString },
+  jasmine: COMMON_OPTIONS,
   'node-tap': COMMON_OPTIONS,
 }
 

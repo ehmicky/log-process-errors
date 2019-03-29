69dc5ad100d87d08c0373e5258096c8d7322cecd
ehmicky <ehmicky@gmail.com>
2019-03-29 10:00:00 +0100

    Fix nyc conflict with node-tap

--- test/helpers/runners.js
+++ test/helpers/runners.js
@@ -1,5 +1,20 @@
 'use strict'
 
+const {
+  env: { SPAWN_WRAP_SHIM_ROOT },
+} = require('process')
+
+const shouldKeep = function({ skip }) {
+  return skip === undefined || !skip()
+}
+
+// `spawn-wrap` monkey patches `child_process`. That library is used by `nyc`.
+// This somehow impacts the output of `node-tap`. The best workaround we have
+// found is to skip `node-tap` testing in CI (which uses `nyc`).
+const usesSpawnWrap = function() {
+  return Boolean(SPAWN_WRAP_SHIM_ROOT)
+}
+
 // We test each runner + reporter combination
 const RUNNERS = [
   { name: 'ava', testing: 'ava', command: helperFile => `ava ${helperFile}` },
@@ -22,14 +37,16 @@ const RUNNERS = [
     testing: 'node-tap',
     command: helperFile =>
       `tap -R=classic ${helperFile.replace('node-tap', 'node_tap')}`,
+    skip: usesSpawnWrap,
   },
   {
     name: 'node-tap:tap',
     testing: 'node-tap',
     command: helperFile =>
       `tap -R=tap ${helperFile.replace('node-tap', 'node_tap')}`,
+    skip: usesSpawnWrap,
   },
-]
+].filter(shouldKeep)
 
 module.exports = {
   RUNNERS,

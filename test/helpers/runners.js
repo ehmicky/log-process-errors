81e9d3adfd0a08f357250ea7347250d8583ded07
ehmicky <ehmicky@gmail.com>
2019-03-29 10:00:00 +0100

    Add comment

--- test/helpers/runners.js
+++ test/helpers/runners.js
@@ -11,6 +11,7 @@ const shouldKeep = function({ skip }) {
 // `spawn-wrap` monkey patches `child_process`. That library is used by `nyc`.
 // This somehow impacts the output of `node-tap`. The best workaround we have
 // found is to skip `node-tap` testing in CI (which uses `nyc`).
+// Would be fixed if https://github.com/tapjs/node-tap/issues/497 is done.
 const usesSpawnWrap = function() {
   return Boolean(SPAWN_WRAP_SHIM_ROOT)
 }

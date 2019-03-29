a940b995a20a038982205513de6e97a06be2df91
ehmicky <ehmicky@gmail.com>
2019-03-29 10:00:00 +0100

    Add comment

--- register/node-tap.js
+++ register/node-tap.js
@@ -1,3 +1,5 @@
+// istanbul ignore file
+// Look for `spawn-wrap` in the codebase to see why Istanbul cannot cover this.
 /* eslint-disable filenames/match-regex, unicorn/filename-case */
 // Meant to be performed as `node -r log-process-errors/register/node-tap`
 // or `require('log-process-errors/register/node-tap')`.

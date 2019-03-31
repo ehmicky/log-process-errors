e452b72019eca0fcb97115542d5ecb259619ab3b
ehmicky <ehmicky@gmail.com>
2019-03-31 10:00:00 +0200

    Add comment

--- src/log.js
+++ src/log.js
@@ -4,6 +4,8 @@
 const defaultLog = function(error, level) {
   // Note that `console` should be referenced inside this function, not outside,
   // as user might monkey patch it.
+  // We defined `error[util.custom.inspect]`, so `error` will use it for pretty
+  // printing.
   // eslint-disable-next-line no-restricted-globals, no-console
   console[level](error)
 }

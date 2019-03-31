000eadec0c373b4204a266391d0ebe5760982b90
ehmicky <ehmicky@gmail.com>
2019-03-31 10:00:00 +0200

    Fix tests

--- test/colors.js
+++ test/colors.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { inspect } = require('util')
+
 const test = require('ava')
 const hasAnsi = require('has-ansi')
 const supportsColor = require('supports-color')
@@ -7,18 +9,20 @@ const supportsColor = require('supports-color')
 const { repeatEvents, startLogging } = require('./helpers')
 
 repeatEvents((prefix, { name, emitEvent }) => {
-  test(`${prefix} should colorize the error message`, async t => {
+  test(`${prefix} should colorize the error`, async t => {
     const { stopLogging, log } = startLogging({ log: 'spy', name })
 
     await emitEvent()
 
     t.true(log.calledOnce)
-    t.is(hasAnsi(log.firstCall.args[0]), Boolean(supportsColor.stdout))
+    t.false(hasAnsi(String(log.firstCall.args[0])))
+    t.false(hasAnsi(log.firstCall.args[0].stack))
+    t.is(hasAnsi(inspect(log.firstCall.args[0])), Boolean(supportsColor.stdout))
 
     stopLogging()
   })
 
-  test(`${prefix} should allow disabling colorizing the error message`, async t => {
+  test(`${prefix} should allow disabling colorizing the error`, async t => {
     const { stopLogging, log } = startLogging({
       log: 'spy',
       colors: false,
@@ -28,7 +32,7 @@ repeatEvents((prefix, { name, emitEvent }) => {
     await emitEvent()
 
     t.true(log.calledOnce)
-    t.false(hasAnsi(log.firstCall.args[0]))
+    t.false(hasAnsi(inspect(log.firstCall.args[0])))
 
     stopLogging()
   })

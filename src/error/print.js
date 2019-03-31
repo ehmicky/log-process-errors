d3c4e84d67abdbae0116ca4bc08456702b9d3baa
ehmicky <ehmicky@gmail.com>
2019-03-31 10:00:00 +0200

    Refactoring

--- src/error/print.js
+++ src/error/print.js
@@ -6,26 +6,37 @@ const { getChalk } = require('../colors')
 
 // Pretty-print error on the console (which uses `util.inspect()`)
 const printError = function({ opts: { colors }, level, name, message, stack }) {
-  const [messageA, ...details] = message.split(':')
-  const detailsA = details.join(':')
-
   const {
     chalk,
-    chalk: { bold, dim, inverse, italic },
+    chalk: { dim },
   } = getChalk(colors)
 
-  // Add color, sign and `event.name` to first message line, and concatenate
-  // `firstLine`
-  const { COLOR, SIGN } = LEVELS[level]
-  const header = chalk[COLOR](
-    `${inverse(
-      bold(` ${SIGN}  ${name} ${italic(`(${messageA})`)} `),
-    )}${detailsA}`,
-  )
-
+  const header = getHeader({ level, name, message, chalk })
   const stackA = dim(stack)
-
   return `${header}\n${stackA}`
+}
+
+// Add color, sign and `event.name` to first message line
+const getHeader = function({
+  level,
+  name,
+  message,
+  chalk,
+  chalk: { italic, inverse, bold },
+}) {
+  const { message: messageA, details } = splitMessage({ message })
+
+  const { COLOR, SIGN } = LEVELS[level]
+  const prefix = ` ${SIGN}  ${name} ${italic(`(${messageA})`)} `
+  const header = `${inverse(bold(prefix))}${details}`
+  const headerA = chalk[COLOR](header)
+  return headerA
+}
+
+const splitMessage = function({ message }) {
+  const [messageA, ...details] = message.split(':')
+  const detailsA = details.join(':')
+  return { message: messageA, details: detailsA }
 }
 
 // Each level is printed in a different way

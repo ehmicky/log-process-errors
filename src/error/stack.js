2c265b53b8f287c9c1a8d0c26f6d931775a27976
ehmicky <ehmicky@gmail.com>
2019-03-31 10:00:00 +0200

    Improve error.stack

--- src/error/stack.js
+++ src/error/stack.js
@@ -1,11 +1,13 @@
 'use strict'
 
+// Retrieve `error.stack` by re-using the original error's stack trace
 const getStack = function({ event: { nextValue, value } }) {
   const stack = getEventStack({ nextValue, value })
   const stackA = removeStackHeader({ stack })
   return stackA
 }
 
+// Find the original error's stack trace
 const getEventStack = function({ nextValue, value }) {
   if (nextValue instanceof Error) {
     return nextValue.stack
@@ -15,15 +17,15 @@ const getEventStack = function({ nextValue, value }) {
     return value.stack
   }
 
-  return new Error('').stack
-}
-
-const removeStackHeader = function({ stack }) {
-  return stack.replace(FIRST_LINE_REGEXP, '')
+  return ''
 }
 
 // Remove first line of `Error.stack` as it contains `Error.name|message`,
 // which is already present in the upper error's `message`
+const removeStackHeader = function({ stack }) {
+  return stack.replace(FIRST_LINE_REGEXP, '')
+}
+
 const FIRST_LINE_REGEXP = /.*\n/u
 
 module.exports = {

1effa04b7f12dadf950f155333aec614234f870f
ehmicky <ehmicky@gmail.com>
2019-04-01 10:00:00 +0200

    Test all warnings variants

--- test/helpers/emit/warning.js
+++ test/helpers/emit/warning.js
@@ -6,11 +6,26 @@ const { promisify } = require('util')
 const pSetImmediate = promisify(setImmediate)
 
 // Emit a `warning` event
-const warning = async function() {
-  emitWarning('message', { type: 'WarningType', code: '500', detail: 'Detail' })
+const warning = async function({ all = false } = {}) {
+  const props = all ? WARNING_PROPS : [WARNING_PROPS[0]]
+  props.forEach(createWarning)
 
   await pSetImmediate()
 }
+
+const createWarning = function({ code, detail }, index) {
+  // Ensure events are not skipped because they look like repeated events.
+  const type = index === 0 ? 'WarningType' : `WarningType${index}`
+
+  emitWarning('message', { type, code, detail })
+}
+
+const WARNING_PROPS = [
+  { code: '500', detail: 'Detail' },
+  { code: '500' },
+  { detail: 'Detail' },
+  {},
+]
 
 module.exports = {
   warning,

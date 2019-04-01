17d8993df1b6551d4ad2ddab289a623ea9e4183d
ehmicky <ehmicky@gmail.com>
2019-04-01 10:00:00 +0200

    Refactoring

--- src/options/testing.js
+++ src/options/testing.js
@@ -5,7 +5,7 @@ const { multipleValidOptions } = require('jest-validate')
 const { RUNNERS } = require('./runners')
 
 // Apply `options.testing` which is basically a preset of options.
-const applyTesting = function({ opts, opts: { testing, level, ...optsA } }) {
+const applyTesting = function({ opts, opts: { level, testing } }) {
   if (testing === undefined) {
     return opts
   }
@@ -13,10 +13,10 @@ const applyTesting = function({ opts, opts: { testing, level, ...optsA } }) {
   const testOpts = RUNNERS[testing]
 
   validateTesting({ testOpts, testing })
-  validateTestOpts({ opts: optsA, testing })
+  validateTestOpts({ opts, testOpts, testing })
 
   return {
-    ...optsA,
+    ...opts,
     ...testOpts,
     // Users can override `level.default` but not the ones defined in `testOpts`
     level: { default: 'error', ...level, ...testOpts.level },
@@ -28,27 +28,36 @@ const validateTesting = function({ testOpts, testing }) {
     return
   }
 
+  const runners = Object.keys(RUNNERS).join(', ')
   throw new Error(
-    `Invalid option 'testing' '${testing}': must be one of ${Object.keys(
-      RUNNERS,
-    ).join(', ')}`,
+    `Invalid option 'testing' '${testing}': must be one of ${runners}`,
   )
 }
 
 // Presets override other options. We make sure users do not assume their
 // options are used when they are actually overriden.
-// However we allow overriding preset's `level` so users can filter events.
-const validateTestOpts = function({ opts, testing }) {
-  const [optName] = Object.keys(opts)
+const validateTestOpts = function({ opts, testOpts, testing }) {
+  const forbiddenOpts = Object.keys(testOpts).filter(isForbiddenOpt)
 
-  if (optName === undefined) {
+  const invalidOpt = Object.keys(opts).find(optName =>
+    forbiddenOpts.includes(optName),
+  )
+
+  if (invalidOpt === undefined) {
     return
   }
 
   throw new Error(
-    `Invalid option '${optName}': it must not be defined together with the option 'testing' '${testing}'`,
+    `Invalid option '${invalidOpt}': it must not be defined together with the option 'testing' '${testing}'`,
   )
 }
+
+// We allow overriding preset's `level` so users can filter events.
+const isForbiddenOpt = function(optName) {
+  return !ALLOWED_OPTS.includes(optName)
+}
+
+const ALLOWED_OPTS = ['level']
 
 // Use during options validation
 const getExampleTesting = function() {

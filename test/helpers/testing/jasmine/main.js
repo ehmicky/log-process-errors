045f70bf2e2dfddec99b0072a18f3cbad1e80391
Hongarc <anhhong12121997t@gmail.com>
2019-05-16 09:37:02 +0700

    Use eslint-env instead of  disable `no-undef`

--- test/helpers/testing/jasmine/main.js
+++ test/helpers/testing/jasmine/main.js
@@ -1,11 +1,10 @@
+//* eslint-env jasmine */
 import { EVENTS } from '../../emit/main.js'
 import { getOptions } from '../options.js'
 
 const { name } = getOptions()
 
-// eslint-disable-next-line no-undef
 describe('should make tests fail', () => {
-  // eslint-disable-next-line no-undef
   it(`on ${name}`, () => {
     // eslint-disable-next-line no-empty-function, max-nested-callbacks
     EVENTS[name]().catch(() => {})

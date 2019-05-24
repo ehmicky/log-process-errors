b1065978b82f682e9ed50999cf68a4fa398d9666
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Use test-each on all tests

--- test/options/options.js
+++ test/options/options.js
@@ -1,4 +1,5 @@
 import test from 'ava'
+import testEach from 'test-each'
 import sinon from 'sinon'
 
 import { startLogging } from '../helpers/init.js'
@@ -24,16 +25,25 @@ const INVALID_OPTIONS = [
 
 const WARNED_OPTIONS = [{ unknown: true }, { level: { unknown: 'error' } }]
 
-INVALID_OPTIONS.forEach(options => {
-  test(`${JSON.stringify(options)} should validate options`, t => {
+const normalizeJestValidate = function(message) {
+  const messageA = normalizeMessage(message, { colors: false })
+  // When using nyc, example function body adds dynamic instrumentation code
+  const messageB = messageA.replace(FUNC_BODY_REGEXP, '$1$2')
+  return messageB
+}
+
+const FUNC_BODY_REGEXP = /\s*(\(\)(?:=>)?\{)[^}]*(\})/gu
+
+testEach(INVALID_OPTIONS, ({ name }, options) => {
+  test(`should validate options | ${name}`, t => {
     const error = t.throws(startLogging.bind(null, options))
 
     t.snapshot(normalizeJestValidate(error.message))
   })
 })
 
-WARNED_OPTIONS.forEach(options => {
-  test(`${JSON.stringify(options)} should warn on options`, t => {
+testEach(WARNED_OPTIONS, ({ name }, options) => {
+  test(`should warn on options | ${name}`, t => {
     // eslint-disable-next-line no-restricted-globals
     const stub = sinon.stub(console, 'warn')
 
@@ -46,12 +56,3 @@ WARNED_OPTIONS.forEach(options => {
     stub.restore()
   })
 })
-
-const normalizeJestValidate = function(message) {
-  const messageA = normalizeMessage(message, { colors: false })
-  // When using nyc, example function body adds dynamic instrumentation code
-  const messageB = messageA.replace(FUNC_BODY_REGEXP, '$1$2')
-  return messageB
-}
-
-const FUNC_BODY_REGEXP = /\s*(\(\)(?:=>)?\{)[^}]*(\})/gu

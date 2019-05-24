2e4452ed404e1dc1b775bab0487563a2065bb0c5
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Refactoring

--- test/helpers/runners.js
+++ test/helpers/runners.js
@@ -6,12 +6,8 @@ const { SPAWN_WRAP_SHIM_ROOT } = env
 // This somehow impacts the output of `node-tap`. The best workaround we have
 // found is to skip `node-tap` testing in CI (which uses `nyc`).
 // Would be fixed if https://github.com/tapjs/node-tap/issues/497 is done.
-const usesSpawnWrap = function() {
-  return Boolean(SPAWN_WRAP_SHIM_ROOT)
-}
-
-const shouldKeep = function({ skip }) {
-  return skip === undefined || !skip()
+const shouldKeep = function({ runner }) {
+  return !(runner.startsWith('node-tap') && SPAWN_WRAP_SHIM_ROOT)
 }
 
 const addName = function({ runner, ...runnerInfo }) {
@@ -36,14 +32,12 @@ export const RUNNERS = [
     runner: 'node-tap:default',
     command: file =>
       `tap --no-coverage ${file.replace('node-tap', 'node_tap')}`,
-    skip: usesSpawnWrap,
   },
   {
     runner: 'node-tap:tap',
     command: file =>
       `tap -R=tap --no-coverage ${file.replace('node-tap', 'node_tap')}`,
     env: { TAP_DIAG: '0' },
-    skip: usesSpawnWrap,
   },
 ]
   .filter(shouldKeep)

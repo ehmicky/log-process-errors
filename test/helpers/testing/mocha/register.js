b65a644ceba57319bb38753632ed78f1b207c9fc
ehmicky <ehmicky@gmail.com>
2019-05-24 10:00:00 +0200

    Improve tests

--- test/helpers/testing/mocha/register.js
+++ test/helpers/testing/mocha/register.js
@@ -1,6 +1,4 @@
 // eslint-disable-next-line import/no-unassigned-import
-import '../stack.js'
-// eslint-disable-next-line import/no-unassigned-import
 import '../../../../register/mocha.js'
 // eslint-disable-next-line import/no-unassigned-import
 import './main.js'
--- test/helpers/testing/mocha/regular.js
+++ test/helpers/testing/mocha/regular.js
@@ -1,6 +1,4 @@
 // eslint-disable-next-line import/no-unassigned-import
-import '../stack.js'
-// eslint-disable-next-line import/no-unassigned-import
 import '../call.js'
 // eslint-disable-next-line import/no-unassigned-import
 import './main.js'

759acd747aea5f3d5b6ae0e15f2ef68888aab4ee
ehmicky <ehmicky@gmail.com>
2020-04-05 10:59:57 +0200

    Fix handling uncaught exceptions or warnings that are not `Error` instances

--- src/error/stack.js
+++ src/error/stack.js
@@ -1,8 +1,12 @@
 // Retrieve `error.stack` by re-using the original error's stack trace
 // Remove first line of `Error.stack` as it contains `Error.name|message`,
 // which is already present in the upper error's `message`
-export const getStack = function({ stack = '' }) {
-  return stack.replace(FIRST_LINE_REGEXP, '')
+export const getStack = function(mainValue) {
+  if (!(mainValue instanceof Error)) {
+    return ''
+  }
+
+  return mainValue.stack.replace(FIRST_LINE_REGEXP, '')
 }
 
 const FIRST_LINE_REGEXP = /.*\n/u

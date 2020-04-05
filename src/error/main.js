b8f99d13fc8c28489b959e31bef66d0295c98852
ehmicky <ehmicky@gmail.com>
2020-04-05 23:42:13 +0200

    Keep error static properties

--- src/error/main.js
+++ src/error/main.js
@@ -1,22 +1,26 @@
 import { inspect } from 'util'
 
 import { getMessage } from './message.js'
-import { getStack } from './stack.js'
 import { printError } from './print.js'
+import { getEventProps } from './props.js'
+import { getStack } from './stack.js'
 
 const { custom } = inspect
 
 // Retrieve `error` which sums up all information that can be gathered about
 // the event.
 export const getError = function({ name, event }) {
+  const { stack, ...staticProps } = getEventProps(event)
   const message = getMessage({ event, name })
-  const stack = getStack({ event })
-  const error = buildError({ name, message, stack })
-  return { error, stack }
+  const stackA = getStack(stack)
+  const error = buildError({ name, message, stack: stackA, staticProps })
+  return { error, stack: stackA }
 }
 
-const buildError = function({ name, message, stack }) {
+const buildError = function({ name, message, stack, staticProps }) {
   const error = new Error(message)
+  // eslint-disable-next-line fp/no-mutating-assign
+  Object.assign(error, staticProps)
   // `error.name` should not be enumerable, to ensure it is correctly printed.
   // eslint-disable-next-line fp/no-mutating-methods
   Object.defineProperty(error, 'name', {
---
+++ src/error/props.js
@@ -0,0 +1,16 @@
+// If event is an error, retrieve static properties except `name` and `message`
+export const getEventProps = function({ nextValue, value }) {
+  if (nextValue instanceof Error) {
+    return getErrorProps(nextValue)
+  }
+
+  if (value instanceof Error) {
+    return getErrorProps(value)
+  }
+
+  return {}
+}
+
+const getErrorProps = function(error) {
+  return { stack: error.stack, ...error }
+}

0a41c65fe9f24b376b050322b18b125da5e9e32a
ehmicky <ehmicky@gmail.com>
2020-10-18 19:38:57 +0200

    Add comment

--- test/helpers/events/version.js
+++ test/helpers/events/version.js
@@ -2,6 +2,7 @@ import { version } from 'process'
 
 import semver from 'semver'
 
+// TODO: remove after dropping support for Node <12.0.0
 // `--unhandled-rejections` was introduced in Node `12.0.0`
 export const hasUnhandledFlag = function () {
   return semver.gte(version, UNHANDLED_FLAG_V)

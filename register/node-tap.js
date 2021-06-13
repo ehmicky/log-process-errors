3ba9ea4e29d9c11c330268b660a822d0256ede17
ehmicky <ehmicky@gmail.com>
2021-06-13 17:25:45 +0200

    Fix linting

--- register/node-tap.js
+++ register/node-tap.js
@@ -1,6 +1,6 @@
+/* eslint-disable filenames/match-regex, unicorn/filename-case */
 // istanbul ignore file
 // Look for `spawn-wrap` in the codebase to see why Istanbul cannot cover this.
-/* eslint-disable filenames/match-regex, unicorn/filename-case */
 // Meant to be performed as `node -r log-process-errors/node-tap.js`
 // or `import 'log-process-errors/node-tap.js'`.
 import logProcessErrors from '../src/main.js'

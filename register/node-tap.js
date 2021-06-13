244330a5b57eae9e56294288e1cd18766195781e
ehmicky <ehmicky@gmail.com>
2021-06-13 18:05:53 +0200

    Fix test coverage comment

--- register/node-tap.js
+++ register/node-tap.js
@@ -1,5 +1,4 @@
 /* eslint-disable filenames/match-regex, unicorn/filename-case */
-// istanbul ignore file
 // Look for `spawn-wrap` in the codebase to see why Istanbul cannot cover this.
 // Meant to be performed as `import 'log-process-errors/node-tap.js'`.
 import logProcessErrors from '../src/main.js'

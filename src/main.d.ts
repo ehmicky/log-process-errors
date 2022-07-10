929b085f90a4a8d3c3bc230dd37f5e5509a30a8b
ehmicky <ehmicky@gmail.com>
2022-07-10 21:36:23 +0200

    Add TypeScript types

---
+++ src/main.d.ts
@@ -0,0 +1,151 @@
+/**
+ * Actual logging level
+ */
+export type Level = 'debug' | 'info' | 'warn' | 'error'
+
+/**
+ * Configured logging level
+ */
+export type LevelOption = Level | 'default' | 'silent'
+
+/**
+ * Process error's `name`
+ */
+export type ErrorName =
+  | 'uncaughtException'
+  | 'warning'
+  | 'unhandledRejection'
+  | 'rejectionHandled'
+
+export type Options = Partial<{
+  /**
+   * By default process errors will be logged to the console using
+   * `console.error()`, `console.warn()`, etc.
+   * This behavior can be overridden with the `log` option.
+   *
+   * If logging is asynchronous, the function should return a promise (or use
+   * `async`/`await`). This is not necessary if logging is buffered (like
+   * [Winston](https://github.com/winstonjs/winston)).
+   *
+   * @default `console.error()`, `console.warn()`, etc.
+   *
+   * @example
+   * ```js
+   * logProcessErrors({
+   *   log(error, level, originalError) {
+   *     winstonLogger[level](error.stack)
+   *   },
+   * })
+   * ```
+   */
+  log: (
+    error: Error,
+    level: Level,
+    originalError: Error,
+  ) => Promise<void> | void
+
+  /**
+   * Which log level to use.
+   *
+   * @default { warning: 'warn', default: 'error' }
+   *
+   * @example
+   * ```js
+   * logProcessErrors({
+   *   level: {
+   *     // Use `debug` log level for `uncaughtException` instead of `error`
+   *     uncaughtException: 'debug',
+   *
+   *     // Skip some logs based on a condition
+   *     default(error) {
+   *       return shouldSkip(error) ? 'silent' : 'default'
+   *     },
+   *   },
+   * })
+   * ```
+   */
+  level: {
+    [errorName in ErrorName | 'default']?:
+      | LevelOption
+      | ((error: Error) => LevelOption)
+  }
+
+  /**
+   * Which process errors should trigger `process.exit(1)`:
+   *  - `['uncaughtException', 'unhandledRejection']` is Node.js default
+   *    behavior since Node.js `15.0.0`. Before, only
+   *    [`uncaughtException`](https://nodejs.org/api/process.html#process_warning_using_uncaughtexception_correctly)
+   *    was enabled.
+   *  - use `[]` to prevent any `process.exit(1)`. Recommended if your process
+   *    is long-running and does not automatically restart on exit.
+   *
+   * `process.exit(1)` will only be fired after successfully logging the process
+   * error.
+   *
+   * @default `['uncaughtException', 'unhandledRejection']` for Node
+   * `>= 15.0.0`, `['uncaughtException']` otherwise.
+   *
+   * @example
+   * ```js
+   * logProcessErrors({ exitOn: ['uncaughtException', 'unhandledRejection'] })
+   * ```
+   */
+  exitOn: Level[]
+
+  /**
+   * When running tests, makes them fail if there are any process errors.
+   *
+   * @default undefined
+   *
+   * @example
+   * ```js
+   * import logProcessErrors from 'log-process-errors'
+   * // Should be initialized before requiring other dependencies
+   * logProcessErrors({ testing: 'ava' })
+   *
+   * import test from 'ava'
+   *
+   * // Tests will fail because a warning is triggered
+   * test('Example test', (t) => {
+   *   process.emitWarning('Example warning')
+   *   t.pass()
+   * })
+   * ```
+   */
+  testing: 'ava' | 'mocha' | 'jasmine' | 'tape' | 'node_tap'
+
+  /**
+   * Colorizes messages.
+   *
+   * @default `true` if the output is a terminal.
+   *
+   * @example
+   * ```js
+   * logProcessErrors({ colors: false })
+   * ```
+   */
+  colors: boolean
+}>
+
+/**
+ * Function that can be fired to restore Node.js default behavior.
+ *
+ * @example
+ * ```js
+ * const restore = logProcessErrors(options)
+ * restore()
+ * ```
+ */
+export type Undo = () => void
+
+/**
+ * Improve how process errors are logged.
+ * Should be called as early as possible in the code, before other `import`
+ * statements.
+ *
+ * @example
+ * ```js
+ * logProcessErrors(options)
+ * ```
+ */
+export default function logProcessErrors(options?: Options): Undo
---
+++ src/main.test-d.ts
@@ -0,0 +1,67 @@
+import logProcessErrors, {
+  Options,
+  Level,
+  LevelOption,
+  ErrorName,
+  Undo,
+} from 'log-process-errors'
+import {
+  expectType,
+  expectAssignable,
+  expectNotAssignable,
+  expectError,
+} from 'tsd'
+
+const undo = logProcessErrors()
+expectType<Undo>(undo)
+expectType<void>(undo())
+expectError(undo(true))
+
+logProcessErrors({})
+expectAssignable<Options>({})
+
+expectAssignable<Level>('debug')
+expectNotAssignable<Level>('default')
+expectNotAssignable<Level>('silent')
+expectNotAssignable<Level>('other')
+
+expectAssignable<LevelOption>('debug')
+expectAssignable<LevelOption>('default')
+expectAssignable<LevelOption>('silent')
+expectNotAssignable<LevelOption>('other')
+
+expectAssignable<ErrorName>('warning')
+expectNotAssignable<ErrorName>('other')
+
+logProcessErrors({ log(error: Error, level: Level, originalError: Error) {} })
+logProcessErrors({ async log() {} })
+expectError(logProcessErrors({ log: true }))
+expectError(logProcessErrors({ log(error: boolean) {} }))
+expectError(logProcessErrors({ log: () => true }))
+
+logProcessErrors({ level: {} })
+logProcessErrors({ level: { warning: 'info' } })
+logProcessErrors({ level: { default: 'info' } })
+logProcessErrors({ level: { warning: 'default' } })
+logProcessErrors({ level: { warning: 'silent' } })
+logProcessErrors({ level: { warning: (error: Error) => 'silent' } })
+expectError(logProcessErrors({ level: true }))
+expectError(logProcessErrors({ level: { warning: () => true } }))
+expectError(logProcessErrors({ level: { warning: 'other' } }))
+expectError(logProcessErrors({ level: { other: 'info' } }))
+expectError(
+  logProcessErrors({ level: { warning: (error: boolean) => 'silent' } }),
+)
+
+logProcessErrors({ exitOn: [] })
+logProcessErrors({ exitOn: ['info', 'debug'] })
+expectError(logProcessErrors({ exitOn: true }))
+expectError(logProcessErrors({ exitOn: [true] }))
+expectError(logProcessErrors({ exitOn: ['other'] }))
+
+logProcessErrors({ testing: 'ava' })
+expectError(logProcessErrors({ testing: true }))
+expectError(logProcessErrors({ testing: 'other' }))
+
+logProcessErrors({ colors: true })
+expectError(logProcessErrors({ colors: 'true' }))

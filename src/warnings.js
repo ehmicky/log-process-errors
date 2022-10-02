f7bc486ed5115c364c0cddfeff8bc668c6450ba1
ehmicky <ehmicky@gmail.com>
2022-10-02 21:42:41 +0200

    Refactoring

--- src/warnings.js
+++ src/warnings.js
@@ -6,43 +6,31 @@ import process from 'process'
 // Alternative ways to do it would be to ask users to pass `--no-warnings`
 // CLI flag or `NODE_NO_WARNINGS=1` environment variable. But this is not as
 // developer-friendly.
+// This is a noop if `init()` is called several times
 export const removeWarningListener = function () {
-  if (warningListener === undefined) {
-    return
+  if (warningListener !== undefined) {
+    process.off('warning', warningListener)
   }
-
-  // This will be a noop if `init()` is called several times
-  process.off('warning', warningListener)
 }
 
 // When this module is undone, Node.js default `warning` listener is restored
+// Do not restore if there is some user-defined listener, including if
+// `init()` was called several times.
 export const restoreWarningListener = function () {
-  if (warningListener === undefined) {
-    return
+  if (warningListener !== undefined && getWarningListeners().length === 0) {
+    process.on('warning', warningListener)
   }
-
-  // Do not restore if there is some user-defined listener, including if
-  // `init()` was called several times.
-  if (getWarningListeners().length !== 0) {
-    return
-  }
-
-  process.on('warning', warningListener)
 }
 
 // We assume the first `warning` listener is the Node.js default one.
 // Checking the function itself makes it rely on internal Node.js code, which
 // is brittle.
-// This can return `undefined` if `--no-warnings` was used
+// This can return an empty array if `--no-warnings` is used.
 // This needs be done at load time to ensure:
 //  - we are not catching user-defined listeners
 //  - this is idempotent, allowing this module to be called several times
-const getWarningListener = function () {
-  return getWarningListeners()[0]
-}
-
 const getWarningListeners = function () {
   return process.listeners('warning')
 }
 
-const warningListener = getWarningListener()
+const [warningListener] = getWarningListeners()

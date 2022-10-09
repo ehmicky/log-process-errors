1c41a2fe80b1f5865c2a7052c3ab6dc317d52674
ehmicky <ehmicky@gmail.com>
2022-10-09 23:23:27 +0200

    Fix tests

--- test/helpers/version.js
+++ test/helpers/version.js
@@ -17,10 +17,3 @@ const PROMISE_REJECTION_EVENTS = new Set([
 ])
 
 const NEW_EXIT_MIN_VERSION = '15.0.0'
-
-// Stack traces include inline preview since this version
-export const hasInlinePreview = function () {
-  return semver.gte(version, INLINE_PREVIEW_VERSION)
-}
-
-const INLINE_PREVIEW_VERSION = '18.8.0'
--- test/log.js
+++ test/log.js
@@ -1,26 +1,12 @@
-import { inspect } from 'util'
-
 import test from 'ava'
 import logProcessErrors from 'log-process-errors'
 import sinon from 'sinon'
 import { each } from 'test-each'
 
 import { EVENTS } from './helpers/events.js'
-import { startLogging } from './helpers/init.js'
-import { normalizeMessage } from './helpers/normalize.js'
 import { removeProcessListeners } from './helpers/remove.js'
-import { hasInlinePreview } from './helpers/version.js'
 
 removeProcessListeners()
-
-const snapshotArgs = function ([error, event]) {
-  return [
-    normalizeMessage(inspect(error)),
-    String(error),
-    Object.keys(error),
-    event,
-  ]
-}
 
 each(EVENTS, ({ title }, { eventName, emit }) => {
   test.serial(`should fire opts.log() | ${title}`, async (t) => {
@@ -34,48 +20,26 @@ each(EVENTS, ({ title }, { eventName, emit }) => {
       exit: false,
     })
 
-    t.true(log.notCalled)
-
+    t.false(log.called)
     await emit()
-
     t.true(log.called)
     t.is(log.callCount, 1)
+    t.true(log.args[0][0] instanceof Error)
 
     stopLogging()
   })
 
-  if (hasInlinePreview()) {
-    test.serial(
-      `should fire opts.log() with arguments | ${title}`,
-      async (t) => {
-        const { stopLogging, log } = startLogging({ spy: true, eventName })
-
-        await emit({ all: true })
-
-        t.true(log.called)
-
-        const snapshot = log.args.flatMap(snapshotArgs)
-        t.snapshot(snapshot)
-
-        stopLogging()
-      },
-    )
-  }
-})
-
-each(EVENTS, ({ title }, { eventName, emit }) => {
   test.serial(`should log on the console by default | ${title}`, async (t) => {
     // eslint-disable-next-line no-restricted-globals
     const stub = sinon.stub(console, 'error')
+    const stopLogging = logProcessErrors({ exit: false })
 
-    const { stopLogging } = startLogging({ log: 'default', eventName })
-
+    t.false(stub.called)
     await emit()
-
-    t.is(stub.callCount, 1)
+    t.is(stub.callCount, eventName === 'rejectionHandled' ? 2 : 1)
+    t.true(stub.args[stub.args.length - 1][0] instanceof Error)
 
     stopLogging()
-
     stub.restore()
   })
 })

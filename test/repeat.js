95cb36d116a6753e6f86bf1d6b239a64d2b6e8b9
ehmicky <ehmicky@gmail.com>
2022-10-09 23:44:44 +0200

    Fix test

--- test/repeat.js
+++ test/repeat.js
@@ -8,20 +8,15 @@ import { removeProcessListeners } from './helpers/remove.js'
 
 removeProcessListeners()
 
-each(EVENTS, ({ title }, { emit }) => {
-  test.serial.only(
-    `should not repeat identical events | ${title}`,
-    async (t) => {
-      const log = sinon.spy()
-      const stopLogging = logProcessErrors({ log, exit: false })
+each(EVENTS, ({ title }, { emitMany, eventName }) => {
+  test.serial(`should not repeat identical events | ${title}`, async (t) => {
+    const log = sinon.spy()
+    const stopLogging = logProcessErrors({ log, exit: false })
 
-      t.is(log.callCount, 0)
-      await emit()
-      t.is(log.callCount, 1)
-      await emit()
-      t.is(log.callCount, 1)
+    t.is(log.callCount, 0)
+    await emitMany(2)
+    t.is(log.callCount, eventName === 'rejectionHandled' ? 2 : 1)
 
-      stopLogging()
-    },
-  )
+    stopLogging()
+  })
 })
